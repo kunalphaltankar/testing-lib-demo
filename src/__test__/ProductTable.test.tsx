db4310162125f0c69f7dc6f81b1a5f06832242aa
Kunal Phaltankar <Kunal.Phaltankar@ibm.com>
2022-10-04 13:47:01 +0530

    ProductTable tests completed

--- src/__test__/ProductTable.test.tsx
+++ src/__test__/ProductTable.test.tsx
@@ -1,16 +1,29 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ProductTable from '../components/ProductTable';
 import PRODUCTS from '../models/stub/PRODUCTS';
 
 describe('ProductTable', () => {
   it('should render with products', () => {
-    const { container } = render(
-      <ProductTable products={PRODUCTS} />,
-    );
+    const { container } = render(<ProductTable products={PRODUCTS} />);
     expect(container).toMatchSnapshot();
   });
-  
-  it.todo('should show only stocked products');
-  it.todo('should filter all products');
-  it.todo('should filter only stocked products');
+
+  it('should show only stocked products', () => {
+    render(<ProductTable products={PRODUCTS} inStockOnly />);
+    const nonStockedProduct = screen.queryByText(/Basketball/i);
+    expect(nonStockedProduct).toBeNull();
+    expect(nonStockedProduct).not.toBeInTheDocument();
+  });
+
+  it('should filter all products named Basketball', () => {
+    render(<ProductTable products={PRODUCTS} filterText="Basketball" />);
+    const basketballProducts = screen.getAllByText(/Basketball/);
+    expect(basketballProducts).toHaveLength(1);
+  });
+
+  it('should filter only stocked products named Basketball', ()=> {
+    render(<ProductTable products={PRODUCTS} inStockOnly filterText="Basketball" />);
+    const basketballProducts = screen.queryAllByText(/Basketball/);
+    expect(basketballProducts).toHaveLength(0);
+  });
 });
