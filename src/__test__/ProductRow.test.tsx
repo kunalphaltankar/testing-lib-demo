44aeceade111118913020062e926a0554a9d65d3
Kunal Phaltankar <Kunal.Phaltankar@ibm.com>
2022-10-06 09:34:14 +0530

    Container updated in Product Row

--- src/__test__/ProductRow.test.tsx
+++ src/__test__/ProductRow.test.tsx
@@ -5,26 +5,20 @@ import PRODUCTS from '../models/stub/PRODUCTS';
 const product = PRODUCTS[1];
 const notInStockProduct = PRODUCTS[2];
 
+const tbody = document.createElement('tbody');
+
 describe('Product Row', () => {
   it('should render stocked product', () => {
-    const { container } = render(
-      <table>
-        <tbody>
-          <ProductRow product={product} />
-        </tbody>
-      </table>,
-    );
+    const { container } = render(<ProductRow product={product} />, {
+      container: document.body.appendChild(tbody),
+    });
     expect(container).toMatchSnapshot();
   });
 
   it('should render with unstocked product', () => {
-    const { container } = render(
-      <table>
-        <tbody>
-          <ProductRow product={notInStockProduct} />
-        </tbody>
-      </table>,
-    );
+    const { container } = render(<ProductRow product={notInStockProduct} />, {
+      container: document.body.appendChild(tbody),
+    });
 
     expect(container).toMatchSnapshot();
 
